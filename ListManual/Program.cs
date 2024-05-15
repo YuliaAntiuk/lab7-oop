6e195dbacf65164e870084580047d94a49762d47
Yulia Antiuk <106272552+YuliaAntiuk@users.noreply.github.com>
2024-05-15 14:43:52 +0300

    Add files via upload

---
+++ ListManual/Program.cs
@@ -0,0 +1,33 @@
+﻿using LinkedList;
+
+namespace ListManual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList lst = new CustomLinkedList(new short[] { 2, 8, 5, 9, 7, 1 });
+            PrintList(lst);
+            int multIndex = lst.FindFirstMultiple(2);
+            Console.WriteLine(multIndex);
+
+            short mult = lst[multIndex];
+            Console.WriteLine(mult);
+
+            //lst.ReplaceEvenPositions();
+            //lst.RemoveOddPositions();
+            //PrintList(lst);
+
+            CustomLinkedList greater = lst.GetValuesGreaterThan(5);
+            PrintList(greater);
+        }
+        static void PrintList(CustomLinkedList lst)
+        {
+            for(int i = 0; i < lst.Count; i++)
+            {
+                Console.Write(lst[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
